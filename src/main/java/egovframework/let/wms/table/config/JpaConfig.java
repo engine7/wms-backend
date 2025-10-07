551d44726a51eabae1cc1205fe174dfc4de51927
sklee <engine777719@gmail.com>
2025-10-07 15:22:32 +0900

    JPA 적용

---
+++ src/main/java/egovframework/let/wms/table/config/JpaConfig.java
@@ -0,0 +1,19 @@
+package egovframework.let.wms.table.config;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+@Primary
+public class JpaConfig {
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
